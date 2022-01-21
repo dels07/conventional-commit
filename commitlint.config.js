f1f9f5fde76cb2e3aeed2cddd4316cdf80d65022
Deli Soetiawan <deli.soetiawan@mekari.com>
2022-01-21 16:20:58 +0700

    chore(TM-123): add commitlint rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,22 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional']
-}
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+      'scope-case': [2, 'always', 'upper-case'],
+      'scope-empty': [2, 'never'],
+      'jira-scope': [2, 'always'],
+    },
+    plugins: [
+      {
+        rules: {
+          'jira-scope': ({ scope }) => {
+            const JIRA_TICKET = /^[A-Z]{2,4}-[0-9]{1,4}/g;
+  
+            return [
+              scope && scope.match(JIRA_TICKET),
+              'Your scope should contain JIRA TICKET format',
+            ];
+          },
+        },
+      },
+    ],
+  };
